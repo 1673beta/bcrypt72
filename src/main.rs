15234be624a26126ae4611737d2c2b9f1c6eab09
Esurio <esurio@esurio1673.net>
2024-11-07 22:07:09 +0900

    動く

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bcrypt::{hash, verify, DEFAULT_COST};
+use std::iter::repeat;
+
 fn main() {
-    println!("Hello, world!");
+    let password = "password";
+    let prefix: String = repeat("a").take(72).collect();
+
+    let combined = format!("{}{}", prefix, password);
+    let hashed = hash(&combined, DEFAULT_COST).unwrap();
+
+    let wrong_password = "wrong_password";
+    let wrong_combined = format!("{}{}", prefix, wrong_password);
+
+    let result = verify(&wrong_combined, &hashed);
+    
+    match result {
+        Ok(valid) => {
+            println!("Password: {}", valid);
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
 }
